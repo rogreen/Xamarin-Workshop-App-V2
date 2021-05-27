a5c67361e99a0bf6f4e041cf465f6cf66e73a928
Robert <rgreen2005@msn.com>
2021-05-27 15:43:26 -0600

    Add project files.

---
+++ HoneyDo/HoneyDo/Models/HoneyDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoneyDo.Models
+{
+    public class HoneyDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AssignedTo { get; set; }
+        public string Priority { get; set; }
+        public string Category { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Recurrence { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ HoneyDo/HoneyDo/Services/IDataStore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HoneyDo.Services
+{
+    public interface IDataStore<T>
+    {
+        void Initialize();
+        Task<List<T>> GetItemsAsync();
+        Task<T> GetItemAsync(int id);
+        Task<bool> AddItemAsync(T item);
+        Task<bool> UpdateItemAsync(T item);
+        Task<bool> DeleteItemAsync(int id);
+    }
+}
